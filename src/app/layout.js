d69fbb9e8f0847c282e6067c8ef63047fadc30f1
fryshaun <shaun@frydigital.com>
2024-02-04 10:13:13 -0800

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,6 @@ export default async function RootLayout({ children }) {
       <body>
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
           <main className="flex w-full flex-1 shrink-0 flex-col items-center justify-center px-8 text-center sm:px-20">
-            <h1 className="mb-12 text-5xl font-bold sm:text-6xl">
-              Next.js with <span className="font-black text-green-400">Supabase</span>
-            </h1>
             <AuthProvider accessToken={session?.access_token}>{children}</AuthProvider>
           </main>
         </div>
